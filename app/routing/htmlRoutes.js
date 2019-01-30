3eac8336aa969796a4edabfd3759f845e503ee9a
unknown <whigs93@gmail.com>
2019-01-30 01:27:30 -0600

    undo

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,13 @@
 var path = require("path");
+var express = require("express");
+var htmlrouter = express.Router();
 
-module.exports = function(app) {
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'))
-  });
-};
+htmlrouter.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+htmlrouter.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+module.exports = htmlrouter;
