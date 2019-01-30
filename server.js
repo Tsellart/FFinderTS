c2a1f6152824f016f55bb066aab22671dabb6350
unknown <whigs93@gmail.com>
2019-01-29 18:21:23 -0600

    s

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var apirouter = require ("./app/routing/apiRoutes");
-var htmlrouter = require ("./app/routing/htmlRoutes");
+var apiroutes = require ("./app/routing/apiRoutes");
+var htmlroutes = require ("./app/routing/htmlRoutes");
 
-app.use(apirouter);
-app.use(htmlrouter);
+app.use(apiroutes);
+app.use(htmlroutes);
 
 app.listen(PORT, function() {
     console.log("Listening for PORT : " + PORT);
